1b6b62a0181cf5fd99cc03a8c49384dade3d9df7
Amrita25 <amrita.majumdar25@gmail.com>
2021-05-12 21:52:12 +0530

    first commit
    first commit

---
+++ src/main/java/io/springsecurity/jwt/practice/springsecurityjwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package io.springsecurity.jwt.practice.springsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
